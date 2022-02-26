a0878fb2adae23c376f4050e90ea14e82e4e9d97
Celeste <celesteong98@hotmail.com>
2022-02-26 16:26:08 +0800

    full login and registration validation done

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required,  logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 # creates database instance 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 # to connect to database file 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+# Allow app and flask to work together
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# Used to reload user object from user id stored in session
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Table creation 
 class User(db.Model, UserMixin):
@@ -40,7 +51,7 @@ class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Username"})
     # Will be black dots for password input using PasswordField
     password = PasswordField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder":"Password"})
-    submit = SubmitField("Register")
+    submit = SubmitField("Login")
 
 @app.route('/')
 def home():
@@ -49,11 +60,31 @@ def home():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        # Checks if user credentials matches the ones in the database
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        # whenever form is submitted, hashed password will immediately be generate 
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
